8d459c5c5c8878b0e8d7b081b80abe2bb5c538fe
Parth Mittal <100128469@lubbockisd.net>
2022-08-31 10:13:16 -0500

    Initial commit

---
+++ src/MyFirstClass.java
@@ -0,0 +1,8 @@
+public class MyFirstClass {
+    public static void main(String[] args) {
+        System.out.print("Greetings programs!");
+        System.out.println("Welcome to the Grid!");
+    }
+}
+
+
