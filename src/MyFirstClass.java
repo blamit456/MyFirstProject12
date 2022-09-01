41552cc0374b2261cb7d6d1207c2cfb1bb1098ec
100128469 <parth.mittal0115@outlook.com>
2022-09-01 09:20:29 -0500

    Added spaces and additional imperative.

--- src/MyFirstClass.java
+++ src/MyFirstClass.java
@@ -2,6 +2,7 @@ public class MyFirstClass {
     public static void main(String[] args) {
         System.out.print("Greetings programs!");
         System.out.println("Welcome to the Grid!");
+        System.out.println("Grab your ducks!");
     }
 }
 
